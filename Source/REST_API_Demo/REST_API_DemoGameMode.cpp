98df744b5b7c7ad50452110a7d29b8ed2904cc26
Leron Bergelson <leronbergelson@gmail.com>
2021-12-02 13:46:38 -0500

    Set up GET Request in Gamemode Class

--- Source/REST_API_Demo/REST_API_DemoGameMode.cpp
+++ Source/REST_API_Demo/REST_API_DemoGameMode.cpp
@@ -12,4 +12,33 @@ AREST_API_DemoGameMode::AREST_API_DemoGameMode()
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
+	
+	Http = &FHttpModule::Get();
+}
+
+void AREST_API_DemoGameMode::PostLogin(APlayerController* NewPlayer) {
+	Super::PostLogin(NewPlayer);
+
+	UE_LOG(LogTemp, Warning, TEXT("Post Login Running"));
+
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = Http->CreateRequest();
+
+	Request->OnProcessRequestComplete().BindUObject(this, &AREST_API_DemoGameMode::OnProcessRequestComplete);
+	Request->SetURL("http://localhost:8080/api/PlayerData");
+	Request->SetVerb("GET");
+	Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	Request->ProcessRequest();
+	
+	//Get Request throught API passing in player ID
+}
+
+void AREST_API_DemoGameMode::OnProcessRequestComplete(FHttpRequestPtr Request, FHttpResponsePtr Response, bool Success)
+{
+	if (Success) {
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *Response->GetContentAsString());
+
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("FAILED"));
+	}
 }
--- Source/REST_API_Demo/REST_API_DemoGameMode.h
+++ Source/REST_API_Demo/REST_API_DemoGameMode.h
@@ -4,7 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "Http.h"
 #include "REST_API_DemoGameMode.generated.h"
+
+USTRUCT(BlueprintType)
+struct FPlayerData
+{
+	GENERATED_BODY()
+	float XCoord = 0.0f;
+	float YCorrd = 0.0f;
+	float ZCoord = 0.0f;
+};
 
 UCLASS(minimalapi)
 class AREST_API_DemoGameMode : public AGameModeBase
@@ -13,6 +23,11 @@ class AREST_API_DemoGameMode : public AGameModeBase
 
 public:
 	AREST_API_DemoGameMode();
+    virtual void PostLogin(APlayerController* NewPlayer) override;
+
+protected:
+	FHttpModule* Http;
+	void OnProcessRequestComplete(FHttpRequestPtr Request, FHttpResponsePtr Response, bool Success);
 };
 
 
